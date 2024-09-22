e3d14fb167a8cfc406c5dc8fa8982fa4633b6baa
yverma5 <yashu15.1993@gmail.com>
2024-09-23 00:34:08 +0530

    compwny service added

--- ResumeManagement/ResumeManagement/Service/Implemenatation/CompanyService.cs
+++ ResumeManagement/ResumeManagement/Service/Implemenatation/CompanyService.cs
@@ -6,17 +6,8 @@ using ResumeManagement.Service.Interface;
 
 namespace ResumeManagement.Service.Implemenatation
 {
-    public class CompanyService : ICompanyService
+    public class CompanyService(ApplicationDbContext context, IMapper mapper) : ICompanyService
     {
-        private readonly ApplicationDbContext context;
-        private readonly IMapper mapper;
-
-        public CompanyService(ApplicationDbContext context,IMapper mapper)
-        {
-            this.context = context;
-            this.mapper = mapper;
-        }
-
         public async Task<bool> CreateCompany(CreateCompanyDto dto)
         {
             try
@@ -28,7 +19,6 @@ namespace ResumeManagement.Service.Implemenatation
             }
             catch (Exception)
             {
-
                 return false;
             }
         }
